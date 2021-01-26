b5269d307eb5660e64d5db909fb4ae58669119fb
JHLEE <jhlee@ubuntu.localdomain>
2021-01-26 23:47:17 +0900

    feature: initial implementation of cat

---
+++ futsu_no_linux_programming_2nd/cat/cat.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+static void do_cat(const char *path);
+static void print_error(const char *s);
+
+int main(int argv, int **args) {
+	int i;
+	if (argv < 2) {
+		fprintf(stderr, "%s file name is not given");
+		exit(1);
+	}
+	
+	for (int i = 0; i < argv; i++) {
+		do_cat(args[i]);
+	}
+
+}
+
+#define BUFFER_SIZE 2048
+
+static void do_cat(const char *path) {
+	int fd;
+	unsigned char buff[BUFFER_SIZE];
+	int word_num = 0;
+
+	fd = open(path, O_RDONLY);
+	if (fd < 0) {
+		print_error("invalid path or file name");
+	}
+
+	while (1) {
+		if (word_num = read(fd, buff, sizeof(buff) < 0)) print_error(path);
+		if (word_num == 0) break;
+		if (write(1, buff, word_num) < 0) print_error(path);
+		
+	}
+	if (close(fd) < 0) print_error(path);
+}
+
+static void print_error(const char *s) {
+	perror(s);
+	exit(1);
+}
